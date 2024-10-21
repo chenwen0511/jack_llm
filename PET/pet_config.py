f7608cc370438ea5da82b40b3d71897daa177a1f
jack.chen <chenwen0511@126.com>
2024-10-21 23:27:31 +0800

    PET readme

--- PET/pet_config.py
+++ PET/pet_config.py
@@ -7,11 +7,11 @@ import sys
 class ProjectConfig(object):
     def __init__(self):
         self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu' # windows电脑/linux服务器
-        self.pre_model = r'D:\01-大模型\大模型训练营--课件\大模型训练营--课件\第七章：新零售行业评价决策系统\01-code\预训练模型\bert-base-chinese'
-        self.train_path = r'D:\01-大模型\大模型训练营--课件\大模型训练营--课件\第七章：新零售行业评价决策系统\01-code\PET\data\train.txt'
-        self.dev_path = r'D:\01-大模型\大模型训练营--课件\大模型训练营--课件\第七章：新零售行业评价决策系统\01-code\PET\data\dev.txt'
-        self.prompt_file = r'D:\01-大模型\大模型训练营--课件\大模型训练营--课件\第七章：新零售行业评价决策系统\01-code\PET\data\prompt.txt'
-        self.verbalizer = r'D:\01-大模型\大模型训练营--课件\大模型训练营--课件\第七章：新零售行业评价决策系统\01-code\PET\data\verbalizer.txt'
+        self.pre_model = r'D:\06-github\jack_llm\PET\bert-base-chinese'
+        self.train_path = r'D:\06-github\jack_llm\PET\data\train.txt'
+        self.dev_path = r'D:\06-github\jack_llm\PET\data\dev.txt'
+        self.prompt_file = r'D:\06-github\jack_llm\PET\data\prompt.txt'
+        self.verbalizer = r'D:\06-github\jack_llm\PET\data\verbalizer.txt'
         self.max_seq_len = 512
         self.batch_size = 8
         self.learning_rate = 5e-5
@@ -21,7 +21,7 @@ class ProjectConfig(object):
         self.epochs = 2
         self.logging_steps = 10
         self.valid_steps = 20
-        self.save_dir = r'D:\01-大模型\大模型训练营--课件\大模型训练营--课件\第七章：新零售行业评价决策系统\01-code\PET\checkpoints'
+        self.save_dir = r'D:\06-github\jack_llm\PET\checkpoints'
 
 
 if __name__ == '__main__':
